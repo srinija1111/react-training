8750832726e1b261e1270e670d55cac969c283ed
sriija1111 <2211cs010544@mallareddyuniversity.ac.in>
2025-02-17 13:00:27 +0530

    adding files

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,58 +1,66 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const Dashboard = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-    gender: "",
-    address: "",
-    file: null,
-    country: ""
-  });
+  const [username ,setUsername] =useState('');
+  const [mail ,setMail] =useState('');
+  const [psw ,setPsw] =useState('');
+  const [gender ,setGender] =useState('');
+  const [address ,setAddress] =useState('');
+  const [country ,setCountry] =useState('');
+  const [file ,setFile] =useState('');
 
-  const handleChange = (e) => {
-    const { name, value, type, files } = e.target;
-    setFormData({
-      ...formData,
-      [name]: type === "file" ? files[0] : value
-    });
-  };
+  let handleGenderChange=e=>{
+    setGender(e.target.value)
+  }
 
+  let handleSubmit=e=>{
+    e.preventDefault();
+    console.log({username , mail , psw ,gender , address , country , file});
+  }
   return (
     <div>
-      <h2>Dashboard</h2>
-      <div className="form-container" style={{ width: "300px", padding: "20px", border: "2px solid black" }}>
-        <input type="text" name="username" placeholder="Username" onChange={handleChange} />
-        <input type="email" name="email" placeholder="Email" onChange={handleChange} />
-        <input type="password" name="password" placeholder="Password" onChange={handleChange} />
-        
-        <div className="gender-container" style={{ display: "flex", alignItems: "center", gap: "10px" }}>
-          <label>Gender:</label>
-          <input type="radio" name="gender" value="male" onChange={handleChange} /> Male
-          <input type="radio" name="gender" value="female" onChange={handleChange} /> Female
-          <input type="radio" name="gender" value="other" onChange={handleChange} /> Other
-        </div>
-        
-        <textarea name="address" placeholder="Address" onChange={handleChange}></textarea>
-        
-        <div className="file-input" style={{ display: "flex", alignItems: "center", gap: "10px" }}>
-          <label>File:</label>
-          <input type="file" name="file" onChange={handleChange} />
-        </div>
-      </div>
-      
-      <br />
-      
-      <select name="country" onChange={handleChange}>
-        <option value="">Country</option>
-        <option value="USA">USA</option>
-        <option value="India">India</option>
-        <option value="UK">UK</option>
-        <option value="Canada">Canada</option>
-      </select>
-    </div>
-  );
-};
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='username'
+        value={username}
+        onChange={(e)=>setUsername(e.target.value)}
+        /> <br/>
+        <input type="email" placeholder='Email'
+         value={mail}
+         onChange={(e)=>setMail(e.target.value)}
+        /> <br/>
+        <input type="password" placeholder='password'
+        value={psw}
+        onChange={(e)=>setPsw(e.target.value)}
+        /> <br/>
 
-export default Dashboard;
+        <input type="radio"  name='gender' checked={gender === "male"} value="male" onChange={handleGenderChange}/><label htmlFor="">Male</label>
+        <input type="radio"  name='gender' checked={gender === "female"} value="female" onChange={handleGenderChange}/><label htmlFor="">Female</label>
+        <input type="radio"  name='gender' checked={gender === "others"} value="others" onChange={handleGenderChange}/><label htmlFor="">Others</label>
+        <br />
+        <textarea name="" id="add" 
+        value={address}
+        onChange={(e)=>setAddress(e.target.value)}
+        ></textarea>
+        <br />
+        <select name="" id="country"
+        value={country}
+        onChange={(e)=>setCountry(e.target.value)}
+        >
+          <option value="">---country---</option>
+          <option value="india">India</option>
+          <option value="pakisthan">Pakisthan</option>
+          <option value="China">China</option>
+        </select>
+        <br />
+        <input type="file" 
+        value={file}
+        onChange={(e)=>setFile(e.target.value)}
+        />
+        <br />
+        <input type="submit" value="SignUp" />
+      </form>
+    </div>
+  )
+}
+
+export default Dashboard
